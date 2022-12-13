23a13f4a3d2bdb9fed17e7b487fad10582447310
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2022-12-13 09:49:09 +0100

    refactor: updated angular dependencies (#53)
    * refactor: updated angular dependencies
    
    Co-authored-by: NicolasMerget <Nicolas.Merget@deutschebahn.com>

---
+++ packages/components/output/vue/vue3/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			name: 'DB-UI',
+			fileName: (format) => `index.${format}.js`
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// Add external deps here
+				globals: {
+					vue: 'Vue'
+				}
+			}
+		}
+	},
+	plugins: [vue()]
+});
