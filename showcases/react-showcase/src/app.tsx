1351e1e3fbfac01579c743de5488a0acfb779d4b
Anna Schoderer <anna.schoderer@deutschebahn.com>
2023-01-06 09:19:26 +0100

    feat: added missing attributes

--- showcases/react-showcase/src/app.tsx
+++ showcases/react-showcase/src/app.tsx
@@ -24,6 +24,17 @@ const App = () => (
 				gap: '1rem',
 				margin: '1rem 0'
 			}}>
+
+			<section className="db-ui-expressive">
+				<DBInput
+					description="Das ist die Beschreibung"
+					label="Textlabel"
+					placeholder="irgendein Text"
+					iconBefore="edit"
+					id="input-expr"
+				/>
+			</section>
+
 			<section className="db-ui-expressive">
 				<DBInput
 					description="Das ist die Beschreibung"
@@ -31,15 +42,18 @@ const App = () => (
 					placeholder="irgendein Text"
 					iconBefore="edit"
 					variant="error"
+					id="input-expr-error"
 				/>
 
 				<DBInput
 					description="Das ist die Beschreibung"
-					label="Textlabel"
+					label="Input required"
 					placeholder="irgendein Text"
 					iconBefore="edit"
 					iconAfter="heart"
 					variant="warning"
+					id="input-expr-warning"
+					required="true"
 				/>
 			</section>
 
@@ -48,15 +62,22 @@ const App = () => (
 					label="Textlabel"
 					placeholder="irgendein Text"
 					iconAfter="heart"
+					id="input-reg"
 				/>
 			</section>
 
 			<section className="db-ui-functional">
 				<DBInput label="Textlabel" placeholder="irgendein Text" />
-				<DBInput label="Textlabel" placeholder="irgendein Text" variant="information" />
+				<DBInput
+					label="Textinput disabled"
+					placeholder="irgendein Text"
+					variant="information"
+					id="input-func"
+					disabled="true"
+				/>
 			</section>
-			</div>
+		</div>
 	</main>
-	);
+);
 
 export default App;
