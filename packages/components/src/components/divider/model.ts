f82306b08b23d74a5386ee302f10dc8b6f9dcb7e
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2023-01-25 15:35:33 +0100

    Update model.ts

--- packages/components/src/components/divider/model.ts
+++ packages/components/src/components/divider/model.ts
@@ -3,7 +3,7 @@ import { GlobalProps, GlobalState } from '../../shared/model';
 export type DBDividerDefaultProps = {
 	margin?: 'none' | '_';
 	variant?: 'horizontal' | 'vertical';
-}
+};
 
 export type DBDividerProps = DBDividerDefaultProps & GlobalProps;
 
