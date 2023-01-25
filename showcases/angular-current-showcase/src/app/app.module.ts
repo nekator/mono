846b0ac25b1143a96d2d678a37653fd0f0d2c1d6
Anna Schoderer <anna.schoderer@deutschebahn.com>
2023-01-25 10:04:12 +0100

    fix: typerror

--- showcases/angular-current-showcase/src/app/app.module.ts
+++ showcases/angular-current-showcase/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {
