e5707937f8908e78bef55f32ab37c197db913967
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-01-26 13:52:30 +0100

    feat: add angular example and fix issues for examples (#264)
    * feat: add angular example and fix issues for examples
    
    * fix: changed components build
    
    * fix: removed secondary install
    
    * fix: package-lock.json
    
    * fix: angular dependencies
    
    * fix: dependencies
    
    * Update README.md
    
    * Update README.md
    
    * Update index.html
    
    * Update package.json
    
    * Update examples/angular-example/.vscode/extensions.json
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * Update examples/angular-example/.editorconfig
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * Update examples/angular-example/tsconfig.json
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * Revert "Update examples/angular-example/.editorconfig"
    
    This reverts commit 72717bdc6ace946ef06d6767d267434183a01da8.
    
    * fix: update package-lock.json
    
    * fix: issue with angular dependency
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    Co-authored-by: Maximilian Franzke <maximilian.franzke@deutschebahn.com>

--- packages/components/scripts/post-build/components.js
+++ packages/components/scripts/post-build/components.js
@@ -1,19 +1,40 @@
 module.exports = [
 	{
 		name: 'page',
-		defaultStylePath: 'components/page/page.css'
+		defaultStylePath: 'components/page/page.css',
+		overwrites: {
+			vue: [
+				{
+					from: 'import { DBPageProps, DBPageState } from "./model";',
+					to: ''
+				}
+			]
+		}
 	},
-
 	{
 		name: 'header',
-		defaultStylePath: 'components/header/header.css'
+		defaultStylePath: 'components/header/header.css',
+		overwrites: {
+			vue: [
+				{
+					from: 'import { DBHeaderState, DBHeaderProps } from "./model";',
+					to: ''
+				}
+			]
+		}
 	},
-
 	{
 		name: 'brand',
-		defaultStylePath: 'components/brand/brand.css'
+		defaultStylePath: 'components/brand/brand.css',
+		overwrites: {
+			vue: [
+				{
+					from: 'import { DBBrandState, DBBrandProps } from "./model";',
+					to: ''
+				}
+			]
+		}
 	},
-
 	{
 		name: 'input',
 		defaultStylePath: 'components/input/input.css',
@@ -41,7 +62,6 @@ module.exports = [
 			]
 		}
 	},
-
 	{
 		name: 'divider',
 		defaultStylePath: 'components/divider/divider.css',
@@ -54,7 +74,6 @@ module.exports = [
 			]
 		}
 	},
-
 	{
 		name: 'card',
 		defaultStylePath: 'components/card/card.css',
@@ -64,7 +83,6 @@ module.exports = [
 			]
 		}
 	},
-
 	{
 		name: 'tab-bar',
 		defaultStylePath: 'components/tab-bar/tab-bar.css',
@@ -102,12 +120,10 @@ module.exports = [
 			]
 		}
 	},
-
 	{
 		name: 'tab',
 		defaultStylePath: 'components/tab/tab.css'
 	},
-
 	{
 		name: 'button',
 		defaultStylePath: 'components/button/button.css',
