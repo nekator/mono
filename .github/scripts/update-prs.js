6991a4c9674c8dc2e715d3a34d266551fdc9e8c0
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-02-06 12:57:47 +0100

    feat: add update pr for push to main (#342)
    * feat: add update pr for push to main
    
    * fix: issue from PR

---
+++ .github/scripts/update-prs.js
@@ -0,0 +1,32 @@
+/* Fetch all open PRs and updates them with main branch.*/
+
+module.exports = async ({ github, context }) => {
+	const { repo, owner } = context.repo;
+	const pulls = await github.rest.pulls.list({
+		owner,
+		repo,
+		state: 'open',
+		base: 'main',
+		per_page: 100
+	});
+
+	const nonDraftPulls = pulls.filter((pr) => !pr.draft);
+	let updatedBranches = 0;
+
+	if (nonDraftPulls.length > 0) {
+		for (const pr of nonDraftPulls) {
+			try {
+				await github.rest.pulls.updateBranch({
+					owner,
+					repo,
+					pull_number: pr.number
+				});
+				updatedBranches++;
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}
+
+	return `Updated branches: ${updatedBranches}/${nonDraftPulls.length}`;
+};
