ece907ac996e7f55543aa199cd8bd05d577ec0b9
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2023-03-23 08:40:59 +0100

    refactor: simplified the functionality

--- packages/components/src/components/radio/radio.lite.tsx
+++ packages/components/src/components/radio/radio.lite.tsx
@@ -77,7 +77,7 @@ export default function DBRadio(props: DBRadioProps) {
 		if (props.checked && state.initialized && document && state._id) {
 			const radioElement = document?.getElementById(state._id);
 			if (radioElement) {
-				radioElement.click();
+				radioElement.setAttribute('checked', 'checked');
 				state.initialized = false;
 			}
 		}
