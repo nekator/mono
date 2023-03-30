c54e86e0c318761f8f5ce2e713de63e4e1f4a10b
annsch <annsch@users.noreply.github.com>
2023-03-30 06:35:19 +0000

    deploy: 6d4abbe995a090050276deb1cb06059618924fbb

--- review/docs-card-component/_next/static/chunks/pages/components/card/docs/HTML-1c310373002236c7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3274],{6921:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/docs/HTML",function(){return u(1147)}])}},function(n){n.O(0,[4996,1147,9774,2888,179],function(){return n(n.s=6921)}),_N_E=n.O()}]);
