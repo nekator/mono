745842c2152523978cdba7a018a76b63dacf538e
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2023-04-01 11:49:29 +0200

    fix: iframe resizer plugin path

--- showcases/patternhub/pages/_app.tsx
+++ showcases/patternhub/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Script from 'next/script';
 import '../styles/globals.scss';
 import '../../showcase-styles.css';
 import '@code-hike/mdx/dist/index.css';
@@ -7,11 +7,7 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
-				<script
-					type="text/javascript"
-					src="iframeResizer.contentWindow.min.js"></script>
-			</Head>
+			<Script src="/iframeResizer.contentWindow.min.js" />
 			<Component {...pageProps} />
 		</>
 	);
