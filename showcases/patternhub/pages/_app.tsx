b376194529c9e217a21dadd9d45a453e1fc36fd2
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2023-04-03 14:38:16 +0200

    chore: slight optimization

--- showcases/patternhub/pages/_app.tsx
+++ showcases/patternhub/pages/_app.tsx
@@ -5,15 +5,13 @@ import '@code-hike/mdx/dist/index.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const iframeResizerPath = process.env.NEXT_PUBLIC_BASE_PATH
+		? `${process.env.NEXT_PUBLIC_BASE_PATH}/iframe-resizer/iframeResizer.contentWindow.min.js`
+		: `/iframe-resizer/iframeResizer.contentWindow.min.js`;
+
 	return (
 		<>
-			<Script
-				src={
-					process.env.NEXT_PUBLIC_BASE_PATH
-						? `${process.env.NEXT_PUBLIC_BASE_PATH}/iframe-resizer/iframeResizer.contentWindow.min.js`
-						: `/iframe-resizer/iframeResizer.contentWindow.min.js`
-				}
-			/>
+			<Script src={iframeResizerPath} />
 			<Component {...pageProps} />
 		</>
 	);
