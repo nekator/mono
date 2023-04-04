5b545d6e69bd44237f0e0349e61df619bee14d04
nmerget <nmerget@users.noreply.github.com>
2023-04-04 05:20:02 +0000

    deploy: 2567e7120ad8e2bcffdd80f0dda558680fc648c8

---
+++ review/main/_next/static/chunks/pages/components/icon/docs/Angular-4c6edaa4bebfdeae.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1486],{6426:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/icon/docs/Angular",function(){return u(9791)}])}},function(n){n.O(0,[4996,9791,9774,2888,179],function(){return n(n.s=6426)}),_N_E=n.O()}]);
