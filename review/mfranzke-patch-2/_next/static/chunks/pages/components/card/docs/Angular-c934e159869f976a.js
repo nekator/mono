a882c74503f61ebe661e4acc32fda813a265237c
mfranzke <mfranzke@users.noreply.github.com>
2023-04-12 07:27:22 +0000

    deploy: 0485513baaadef93de5ed4ad64308c8f90c087ef

---
+++ review/mfranzke-patch-2/_next/static/chunks/pages/components/card/docs/Angular-c934e159869f976a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4225],{7298:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/docs/Angular",function(){return u(409)}])}},function(n){n.O(0,[4996,409,9774,2888,179],function(){return n(n.s=7298)}),_N_E=n.O()}]);
