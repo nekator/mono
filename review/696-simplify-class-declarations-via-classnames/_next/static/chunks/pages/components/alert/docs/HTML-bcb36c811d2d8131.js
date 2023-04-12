3a8985ebaf89984058bed9534feadaccc8165314
mfranzke <mfranzke@users.noreply.github.com>
2023-04-12 15:17:28 +0000

    deploy: 707b869e7763cfe530064e99e4eb0d4165082e4f

---
+++ review/696-simplify-class-declarations-via-classnames/_next/static/chunks/pages/components/alert/docs/HTML-bcb36c811d2d8131.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4280],{3445:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/alert/docs/HTML",function(){return u(7281)}])}},function(n){n.O(0,[4996,7281,9774,2888,179],function(){return n(n.s=3445)}),_N_E=n.O()}]);
