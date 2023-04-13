825738daaf5f5df97b5a3f8432c1f9bf61d2c762
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-04-13 12:54:37 +0200

    feat: add migration route to patternhub and simplified routes (#838)

--- showcases/patternhub/pages/_app.tsx
+++ showcases/patternhub/pages/_app.tsx
@@ -3,10 +3,20 @@ import type { AppProps } from 'next/app';
 import '@code-hike/mdx/dist/index.css';
 import '../styles/globals.scss';
 import '../../showcase-styles.css';
+import { MDXProvider } from '@mdx-js/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<MDXProvider
+			components={{
+				a: (props) => (
+					<a
+						target="_blank"
+						referrerPolicy="no-referrer"
+						{...props}
+					/>
+				)
+			}}>
 			<Script
 				src={
 					(process.env.NEXT_PUBLIC_BASE_PATH ?? '') +
@@ -14,6 +24,6 @@ export default function App({ Component, pageProps }: AppProps) {
 				}
 			/>
 			<Component {...pageProps} />
-		</>
+		</MDXProvider>
 	);
 }
