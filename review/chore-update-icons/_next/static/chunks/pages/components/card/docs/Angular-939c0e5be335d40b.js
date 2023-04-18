2642fc0e4ceabf3d07023e3fe866260780eae41c
nmerget <nmerget@users.noreply.github.com>
2023-04-18 00:57:40 +0000

    deploy: 0dca72a1e6958f9dfeea18817c8c5f61c5c0da90

--- review/chore-update-icons/_next/static/chunks/pages/components/card/docs/Angular-939c0e5be335d40b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4225],{57298:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/docs/Angular",function(){return u(40409)}])}},function(n){n.O(0,[4996,409,9774,2888,179],function(){return n(n.s=57298)}),_N_E=n.O()}]);
--- review/mfranzke-patch-2/_next/static/chunks/pages/components/link/code/Variant-b8f1e3e7b04ef08f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9770],{20564:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/link/code/Variant",function(){return u(61756)}])}},function(n){n.O(0,[4996,1756,9774,2888,179],function(){return n(n.s=20564)}),_N_E=n.O()}]);
