2eb6cec6fee56c37087a0206cc7f18f0b88c32a0
nmerget <nmerget@users.noreply.github.com>
2023-04-20 06:12:05 +0000

    deploy: 60689606a7d435325bef68cc20eece00a423a9c6

---
+++ review/refactor-scss-adaptive-variants/_next/static/chunks/pages/components/button/code/Size-0dbf80d162995223.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2885],{34704:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/button/code/Size",function(){return u(9340)}])}},function(n){n.O(0,[4996,9340,9774,2888,179],function(){return n(n.s=34704)}),_N_E=n.O()}]);
