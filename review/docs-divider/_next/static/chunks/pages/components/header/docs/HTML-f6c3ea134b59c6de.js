2a69deb91586b5ef66a45203b833b5db1bf63120
nmerget <nmerget@users.noreply.github.com>
2023-04-21 00:58:14 +0000

    deploy: c88879486b375e458820c093a33ee18b1e2e894b

--- review/docs-divider/_next/static/chunks/pages/components/header/docs/HTML-f6c3ea134b59c6de.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6330],{39849:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/header/docs/HTML",function(){return u(2176)}])}},function(n){n.O(0,[4996,2176,9774,2888,179],function(){return n(n.s=39849)}),_N_E=n.O()}]);
--- review/fix-generate-component/_next/static/chunks/pages/components/button/code/Variant-04480ab0d2436c05.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5270],{2248:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/button/code/Variant",function(){return u(66194)}])}},function(n){n.O(0,[4996,6194,9774,2888,179],function(){return n(n.s=2248)}),_N_E=n.O()}]);
