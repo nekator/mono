0ef7aaf8374f26c7657fc6f8d9be50f68a160739
nmerget <nmerget@users.noreply.github.com>
2023-04-21 06:13:15 +0000

    deploy: 63586934fb7cbcd45adcdcda1610097e46bc04bb

---
+++ review/dependabot-npm_and_yarn-vite-4.3.1/_next/static/chunks/pages/components/page/docs/HTML-6f72be2de08228ee.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[909],{73561:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/page/docs/HTML",function(){return u(84512)}])}},function(n){n.O(0,[4996,4512,9774,2888,179],function(){return n(n.s=73561)}),_N_E=n.O()}]);
