98e24163627ec7c5f4241f5ab1c91f4947a60ad4
nmerget <nmerget@users.noreply.github.com>
2023-04-21 06:12:21 +0000

    deploy: 50715c7575c5cbeaf242179e37820f658d89f2d8

---
+++ review/dependabot-npm_and_yarn-examples-react-example-vitejs-plugin-react-4.0.0/_next/static/chunks/pages/components/section/docs/Angular-44fa8bacc82e713f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7283],{14719:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/section/docs/Angular",function(){return u(45536)}])}},function(n){n.O(0,[4996,5536,9774,2888,179],function(){return n(n.s=14719)}),_N_E=n.O()}]);
