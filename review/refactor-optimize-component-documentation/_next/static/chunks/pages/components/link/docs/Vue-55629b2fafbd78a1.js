2b6ec98138b75d6724b9423dd22bdf512a13a5b3
mfranzke <mfranzke@users.noreply.github.com>
2023-04-22 10:11:05 +0000

    deploy: 11c13830d7b6d87538c014ecac3f7c26305dec5f

---
+++ review/refactor-optimize-component-documentation/_next/static/chunks/pages/components/link/docs/Vue-55629b2fafbd78a1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4209],{39671:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/link/docs/Vue",function(){return u(93331)}])}},function(n){n.O(0,[4996,3331,9774,2888,179],function(){return n(n.s=39671)}),_N_E=n.O()}]);
