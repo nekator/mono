062bf05b6c7d1fdf61ca5481040550ee84a737bc
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-04-27 11:18:11 +0200

    refactor: moved examples to own repo and combine angular showcases (#930)

--- showcases/angular-showcase/src/main.ts
+++ showcases/angular-showcase/src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+	enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((error) => {
-    console.error(error);
-  });
+	.bootstrapModule(AppModule)
+
+	.catch((error) => {
+		console.error(error);
+	});
