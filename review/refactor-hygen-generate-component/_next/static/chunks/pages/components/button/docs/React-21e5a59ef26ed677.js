1b7000ea48581596b575ec153799da2555572950
nmerget <nmerget@users.noreply.github.com>
2023-04-27 12:56:33 +0000

    deploy: e96b14d7b4204219a112839263dcb8150710d115

---
+++ review/refactor-hygen-generate-component/_next/static/chunks/pages/components/button/docs/React-21e5a59ef26ed677.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6879],{37090:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/button/docs/React",function(){return u(64199)}])}},function(n){n.O(0,[4996,4199,9774,2888,179],function(){return n(n.s=37090)}),_N_E=n.O()}]);
