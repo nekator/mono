a0b5d06c3ad01990ea37cd96e99e34ed9bdf43fc
nmerget <nmerget@users.noreply.github.com>
2023-05-04 00:58:55 +0000

    deploy: c36d8ef79799af69179c14b247d817f4a893f960

--- review/fix-issues-from-dependencies/_next/static/chunks/pages/components/card/docs/HTML-ad56ca1dcf4b0e64.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3274],{76921:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/docs/HTML",function(){return u(91147)}])}},function(n){n.O(0,[4996,1147,9774,2888,179],function(){return n(n.s=76921)}),_N_E=n.O()}]);
