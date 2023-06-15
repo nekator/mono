c36f7a193f331743580dd4201d8f7e8c7083db40
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2023-06-15 14:48:39 +0200

    fix: set list attribute conditionally

--- packages/components/src/components/input/input.lite.tsx
+++ packages/components/src/components/input/input.lite.tsx
@@ -102,10 +102,8 @@ export default function DBInput(props: DBInputProps) {
 	});
 
 	onMount(() => {
-		state._id = props.id ? props.id : 'input-' + uuid();
-		state._dataListId = props.dataListId
-			? props.dataListId
-			: `datalist-${state._id}`;
+		state._id = props.id || 'input-' + uuid();
+		state._dataListId = props.dataListId || `datalist-${uuid()}`;
 
 		if (props.stylePath) {
 			state.stylePath = props.stylePath;
@@ -141,7 +139,7 @@ export default function DBInput(props: DBInputProps) {
 				onChange={(event) => state.handleChange(event)}
 				onBlur={(event) => state.handleBlur(event)}
 				onFocus={(event) => state.handleFocus(event)}
-				list={state._dataListId}
+				list={props.dataList && state._dataListId}
 			/>
 			<label
 				htmlFor={state._id}
