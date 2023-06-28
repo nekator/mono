a7d8a3447e050f582e7e4aae8ee933917984f79b
nmerget <nmerget@users.noreply.github.com>
2023-06-28 07:06:36 +0000

    deploy: fa006f7bbb4936c10cc4190927b309e4cca3f862

---
+++ review/feat-simple-html-for-dev/_next/static/chunks/pages/components/page/docs/Angular-a71664b4d9490be4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6904],{75890:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/page/docs/Angular",function(){return u(50143)}])}},function(n){n.O(0,[4996,143,9774,2888,179],function(){return n(n.s=75890)}),_N_E=n.O()}]);
