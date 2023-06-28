d53070477784c4cef6a419be8f47c3bd923a6638
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2023-06-28 09:47:56 +0200

    fix: added missing children property

--- packages/components/src/components/tag/tag.lite.tsx
+++ packages/components/src/components/tag/tag.lite.tsx
@@ -19,6 +19,7 @@ useMetadata({
 		includeIcon: true,
 		isFormComponent: true,
 		properties: [
+			{ name: 'children', type: 'SingleLine.Text' },
 			{ name: 'name', type: 'SingleLine.Text' },
 			// { name: 'disabled', type: 'TwoOptions' },
 			{
