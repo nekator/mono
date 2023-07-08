efc7673d0b799d792a62b8c5dd6d2a9e8569db7b
nmerget <nmerget@users.noreply.github.com>
2023-07-08 01:15:59 +0000

    deploy: 8480923ed2065c4276e164bfb7eb855a0867dca5

--- review/dependabot-npm_and_yarn-prettier-and-prettier-plugin-pkg-and-prettier-plugin-sh-3.0.0/_next/static/chunks/pages/components/checkbox/docs/Angular-95c33c518fabf0c9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8783],{6506:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/checkbox/docs/Angular",function(){return u(49301)}])}},function(n){n.O(0,[4996,9301,9774,2888,179],function(){return n(n.s=6506)}),_N_E=n.O()}]);
