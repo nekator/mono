8a6cd72c0360cc9359fc211e6e5cf5c5fb39ffd5
mfranzke <mfranzke@users.noreply.github.com>
2023-08-23 11:44:52 +0000

    deploy: 141992db975878aa7b171b0cf32270975cdadfd6

---
+++ version/v0.0.37/_next/static/chunks/pages/components/drawer/docs/HTML-42a7182821fe6bd0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[652],{66341:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/drawer/docs/HTML",function(){return u(32423)}])}},function(n){n.O(0,[4996,2423,9774,2888,179],function(){return n(n.s=66341)}),_N_E=n.O()}]);
