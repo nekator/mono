508c2b00d9b7ddcbf00555f00744e27858ce03a9
nmerget <nmerget@users.noreply.github.com>
2023-08-27 12:34:52 +0000

    deploy: d00dc8e083d348c42cdab1d44bcda8edb0b5705a

---
+++ review/refactor-icons/_next/static/chunks/pages/components/main-navigation/docs/Vue-45bd7787497bb506.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4379],{22923:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/main-navigation/docs/Vue",function(){return u(10185)}])}},function(n){n.O(0,[4996,185,9774,2888,179],function(){return n(n.s=22923)}),_N_E=n.O()}]);
---
+++ review/refactor-icons/_next/static/chunks/pages/components/tag/code/Content-34bc3d023af004bc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8494],{17035:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/tag/code/Content",function(){return o(14183)}])}},function(n){n.O(0,[4996,4183,9774,2888,179],function(){return n(n.s=17035)}),_N_E=n.O()}]);
