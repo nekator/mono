4bb9123677606c06d37cce3f676a628d49207cc7
annsch <annsch@users.noreply.github.com>
2023-08-28 12:55:35 +0000

    deploy: 54a6900a6eeb1b7c3be942dcd3a2c48c9b477310

---
+++ version/v0.0.40/_next/static/chunks/pages/components/accordion/docs/React-82d0ccbd821ff76c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1799],{26170:function(n,_,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/accordion/docs/React",function(){return c(66717)}])}},function(n){n.O(0,[4996,6717,9774,2888,179],function(){return n(n.s=26170)}),_N_E=n.O()}]);
