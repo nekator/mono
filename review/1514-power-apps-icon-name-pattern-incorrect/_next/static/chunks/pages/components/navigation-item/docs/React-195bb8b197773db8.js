10eaea03946c02b06a7b1bb0d7bf424076b00f9d
nmerget <nmerget@users.noreply.github.com>
2023-09-02 00:48:29 +0000

    deploy: 446e347bae88f9f4b9ef3d2466cc8f01348c4a0a

--- review/1514-power-apps-icon-name-pattern-incorrect/_next/static/chunks/pages/components/navigation-item/docs/React-195bb8b197773db8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5579],{36027:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/navigation-item/docs/React",function(){return o(36904)}])}},function(n){n.O(0,[4996,7272,9774,2888,179],function(){return n(n.s=36027)}),_N_E=n.O()}]);
