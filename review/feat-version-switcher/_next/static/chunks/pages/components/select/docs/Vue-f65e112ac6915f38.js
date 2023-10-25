c43b41ce249d4139a160e934e0adbfd386db37ae
nmerget <nmerget@users.noreply.github.com>
2023-10-25 09:24:18 +0000

    deploy: 3f221fa1b3bdd310b37c0959faf38ff0ada54777

---
+++ review/feat-version-switcher/_next/static/chunks/pages/components/select/docs/Vue-f65e112ac6915f38.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1074],{40766:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/select/docs/Vue",function(){return u(72647)}])}},function(n){n.O(0,[4996,2647,9774,2888,179],function(){return n(n.s=40766)}),_N_E=n.O()}]);
