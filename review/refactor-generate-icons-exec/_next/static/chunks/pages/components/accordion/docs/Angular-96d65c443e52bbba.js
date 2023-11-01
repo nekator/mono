19b75a4291f390325d6ea9851e8e9d1fc9ee65e4
nmerget <nmerget@users.noreply.github.com>
2023-11-01 00:55:08 +0000

    deploy: 03a229592184a56867bef115da0db3da3d407b91

--- review/refactor-generate-icons-exec/_next/static/chunks/pages/components/accordion/docs/Angular-96d65c443e52bbba.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[68394],{44129:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/accordion/docs/Angular",function(){return o(9151)}])}},function(n){n.O(0,[64996,9151,49774,92888,40179],function(){return n(n.s=44129)}),_N_E=n.O()}]);
--- review/refactor-generate-icons-exec/_next/static/chunks/pages/components/tooltip/docs/Vue-a41d9492d788d457.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[60837],{84953:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/tooltip/docs/Vue",function(){return o(51636)}])}},function(n){n.O(0,[64996,51636,49774,92888,40179],function(){return n(n.s=84953)}),_N_E=n.O()}]);
