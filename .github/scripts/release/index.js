064435d636d0df02b5cdc2df970deaa387502cd3
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-11-14 14:11:47 +0100

    feat: upload icon assets via GitHub Actions

---
+++ .github/scripts/release/index.js
@@ -0,0 +1,51 @@
+/*
+ * Handles full release process for all assets
+ */
+
+import uploadAsset from './upload-asset.js';
+import updateIconRelease from './update-icon-release.js';
+
+import ChildProcess from 'node:child_process';
+
+const release = async ({ github, context, iconReleaseId }) => {
+	const release_id = context.payload.release.id;
+
+	// 1. Upload power-apps asset
+	const assetName = 'DBUI.zip';
+	const assetPath = `${github.workspace}/build-power-apps/DBUI/bin/Release/${assetName}`;
+	await uploadAsset({ github, context, release_id, assetName, assetPath });
+
+	// 2. Update  IconRelease with current tag
+	const updatedIconRelease = await updateIconRelease({
+		github,
+		context,
+		iconReleaseId
+	});
+
+	// 3. Upload latest icon assets
+	const iconsName = 'icons.zip';
+	const iconsPath = `${github.workspace}/packages/foundations/assets`;
+	ChildProcess.execSync(`zip -r ${iconsName} ${iconsPath}/icons`);
+
+	// 3.1 Upload to current release
+	await uploadAsset({
+		github,
+		context,
+		release_id,
+		assetName: iconsName,
+		assetPath: `${iconsPath}/${iconsName}`
+	});
+
+	if (!context.payload.release.prerelease) {
+		// 3.2 Upload to iconRelease -> will overwrite "latest" icons
+		await uploadAsset({
+			github,
+			context,
+			release_id: updatedIconRelease.id,
+			assetName: iconsName,
+			assetPath: `${iconsPath}/${iconsName}`
+		});
+	}
+};
+
+export default release;
---
+++ .github/scripts/release/update-icon-release.js
@@ -0,0 +1,43 @@
+/*
+ * Uploads a file to a release
+ */
+
+const updateIconRelease = async ({ github, context, iconReleaseId }) => {
+	const { repo, owner } = context.repo;
+	let releaseId = iconReleaseId;
+
+	const { tag_name } = await github.rest.repos.getLatestRelease({
+		owner,
+		repo
+	});
+
+	if (releaseId === 'false') {
+		// We use release_id from GitHub variables https://github.com/db-ui/mono/settings/variables/actions/ICON_RELEASE_ID
+		// At the first time we will generate the release via API
+
+		if (latestRelease) {
+			const iconRelease = await github.rest.repos.createRelease({
+				owner,
+				repo,
+				tag_name,
+				body: 'Default icon release for downloading assets',
+				prerelease: false,
+				draft: false,
+				generate_release_notes: false,
+				make_latest: 'false'
+			});
+			if (iconRelease) {
+				releaseId = iconRelease.id;
+			}
+		}
+	}
+
+	return await github.rest.repos.updateRelease({
+		owner,
+		repo,
+		release_id: releaseId,
+		tag_name
+	});
+};
+
+export default updateIconRelease;
