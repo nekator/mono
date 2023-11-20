4c5fc6d7940717032d0f20d6794b2b68fb262c65
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-11-20 15:54:29 +0100

    fix: issue with missing variable

--- .github/scripts/release/update-icon-release.js
+++ .github/scripts/release/update-icon-release.js
@@ -14,21 +14,18 @@ const updateIconRelease = async ({ github, context, iconReleaseId }) => {
 	if (releaseId === 'false') {
 		// We use release_id from GitHub variables https://github.com/db-ui/mono/settings/variables/actions/ICON_RELEASE_ID
 		// At the first time we will generate the release via API
-
-		if (latestRelease) {
-			const iconRelease = await github.rest.repos.createRelease({
-				owner,
-				repo,
-				tag_name,
-				body: 'Default icon release for downloading assets',
-				prerelease: false,
-				draft: false,
-				generate_release_notes: false,
-				make_latest: 'false'
-			});
-			if (iconRelease) {
-				releaseId = iconRelease.id;
-			}
+		const iconRelease = await github.rest.repos.createRelease({
+			owner,
+			repo,
+			tag_name,
+			body: 'Default icon release for downloading assets',
+			prerelease: false,
+			draft: false,
+			generate_release_notes: false,
+			make_latest: 'false'
+		});
+		if (iconRelease) {
+			releaseId = iconRelease.id;
 		}
 	}
 
