a735f431ed3df40cfa65148adc8413082b728bf1
nmerget <nmerget@users.noreply.github.com>
2023-11-23 00:54:17 +0000

    deploy: 455d1d4e5bb6be6a00d65b59eba9ce182f587cba

--- review/1822-icon-update/_next/static/chunks/pages/components/checkbox/docs/Vue-1dfad0a6c5c4cae7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4484],{71321:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/checkbox/docs/Vue",function(){return u(52023)}])}},function(n){n.O(0,[64996,52023,49774,92888,40179],function(){return n(n.s=71321)}),_N_E=n.O()}]);
