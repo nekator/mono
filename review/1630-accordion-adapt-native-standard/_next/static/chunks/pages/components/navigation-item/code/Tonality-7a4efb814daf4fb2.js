1b86d9a83aca84e3229bcae5f2332cbdbfbf5ffc
nmerget <nmerget@users.noreply.github.com>
2023-11-29 14:16:28 +0000

    deploy: 7e12550bb60e0d1db20514e3e86e2fef575148f9

---
+++ review/1630-accordion-adapt-native-standard/_next/static/chunks/pages/components/navigation-item/code/Tonality-7a4efb814daf4fb2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61367],{28023:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/navigation-item/code/Tonality",function(){return o(9803)}])}},function(n){n.O(0,[64996,9803,49774,92888,40179],function(){return n(n.s=28023)}),_N_E=n.O()}]);
