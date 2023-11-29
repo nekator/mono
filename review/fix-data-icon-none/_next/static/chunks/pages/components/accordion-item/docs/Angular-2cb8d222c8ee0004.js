5018d1ffc1121746b71ee41eb739425fbb02482b
nmerget <nmerget@users.noreply.github.com>
2023-11-29 11:28:26 +0000

    deploy: e45fcd5003147faaffce74e172ccbc6a72bedb95

---
+++ review/fix-data-icon-none/_next/static/chunks/pages/components/accordion-item/docs/Angular-2cb8d222c8ee0004.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[15981],{28233:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/accordion-item/docs/Angular",function(){return o(35405)}])}},function(n){n.O(0,[64996,35405,49774,92888,40179],function(){return n(n.s=28233)}),_N_E=n.O()}]);
