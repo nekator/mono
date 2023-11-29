08215b578513f5b4eaf4834752ea5906d7c4864c
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-11-29 17:35:18 +0100

    fix: issue with missing logo (#1927)

--- showcases/vue-showcase/vite.config.ts
+++ showcases/vue-showcase/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: `/vue-showcase`,
+	base: `/vue-showcase/`,
 	plugins: [vue()],
 	build: {
 		outDir: '../../build-showcases/vue-showcase',
