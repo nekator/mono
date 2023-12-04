bc404507c841a91b3e1bb2ba3b4a8d0d09174b78
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-12-04 13:33:39 +0100

    fix: issue with releasing icon assets (#1950)

--- .github/scripts/release/upload-asset.js
+++ .github/scripts/release/upload-asset.js
@@ -11,6 +11,25 @@ const uploadAsset = async ({
 	assetPath
 }) => {
 	const { repo, owner } = context.repo;
+
+	const uploadRelease = await github.rest.repos.getRelease({
+		owner,
+		repo,
+		release_id
+	});
+
+	const foundAsset = uploadRelease?.assets?.find(
+		(asset) => asset.name === assetName
+	);
+
+	if (foundAsset) {
+		await github.rest.repos.deleteReleaseAsset({
+			owner,
+			repo,
+			asset_id: foundAsset.id
+		});
+	}
+
 	return await github.rest.repos.uploadReleaseAsset({
 		owner,
 		repo,
