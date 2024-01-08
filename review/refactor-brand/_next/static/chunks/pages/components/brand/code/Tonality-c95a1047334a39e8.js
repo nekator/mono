41a89753c8c67522885a17edd1499d87cf64ddb9
mfranzke <mfranzke@users.noreply.github.com>
2024-01-08 08:00:45 +0000

    deploy: 1e7ca542fcc88cba207ee6ce11f58525d68cadf7

---
+++ review/refactor-brand/_next/static/chunks/pages/components/brand/code/Tonality-c95a1047334a39e8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[27539],{69664:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/brand/code/Tonality",function(){return o(20767)}])}},function(n){n.O(0,[64996,20767,49774,92888,40179],function(){return n(n.s=69664)}),_N_E=n.O()}]);
