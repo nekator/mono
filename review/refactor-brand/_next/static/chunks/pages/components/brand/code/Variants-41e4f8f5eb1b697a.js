cb2d8d5feb2a0063209ed51c49c31a7f1ba7339d
nmerget <nmerget@users.noreply.github.com>
2024-01-17 00:56:01 +0000

    deploy: 19e11a53122dfd69f8633349e3d44b753ea15392

--- review/refactor-brand/_next/static/chunks/pages/components/brand/code/Variants-41e4f8f5eb1b697a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[81240],{69173:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/brand/code/Variants",function(){return u(22696)}])}},function(n){n.O(0,[64996,22696,49774,92888,40179],function(){return n(n.s=69173)}),_N_E=n.O()}]);
