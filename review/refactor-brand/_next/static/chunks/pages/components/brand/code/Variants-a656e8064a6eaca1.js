263a6b5f23f325ffb64fe26865be6f0393800f96
nmerget <nmerget@users.noreply.github.com>
2024-01-31 13:26:57 +0000

    deploy: db6da3720a3e73adf8e7ef0bcb70d3b8d1b4e1cb

---
+++ review/refactor-brand/_next/static/chunks/pages/components/brand/code/Variants-a656e8064a6eaca1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[81240],{69173:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/brand/code/Variants",function(){return u(22696)}])}},function(n){n.O(0,[64996,22696,92888,49774,40179],function(){return n(n.s=69173)}),_N_E=n.O()}]);
