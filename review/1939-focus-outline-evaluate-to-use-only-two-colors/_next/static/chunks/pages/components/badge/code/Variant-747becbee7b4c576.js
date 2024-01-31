ddef745f7f6e3f825b80ac04d547876214c0f0c4
mfranzke <mfranzke@users.noreply.github.com>
2024-01-31 13:16:34 +0000

    deploy: b404257cd46657761ce9b571357c73d3771fe270

---
+++ review/1939-focus-outline-evaluate-to-use-only-two-colors/_next/static/chunks/pages/components/badge/code/Variant-747becbee7b4c576.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[46417],{57394:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/badge/code/Variant",function(){return u(88424)}])}},function(n){n.O(0,[64996,88424,92888,49774,40179],function(){return n(n.s=57394)}),_N_E=n.O()}]);
