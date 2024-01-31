53730e1dd4b9ac9f331ceaa8d85128d21896ac8b
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-01-31 08:53:42 +0100

    fix: issue with wrong import

--- showcases/patternhub/pages/iframe/index.tsx
+++ showcases/patternhub/pages/iframe/index.tsx
@@ -2,13 +2,13 @@
 import { Buffer } from 'buffer';
 import { useRouter } from 'next/router';
 import ComponentParser from '../../components/component-parser';
-import { COLORS } from '../../../components/src/shared/constants';
+import { COLOR } from '../../components/src/shared/constants';
 
 const IframeComponent = () => {
 	const router = useRouter();
 
 	const tonality = router.query.tonality ?? 'regular';
-	const color = router.query.color ?? COLORS.BASE_BG_1;
+	const color = router.query.color ?? COLOR.BASE_BG_1;
 
 	const componentsString: string = (router.query.components as string) ?? '';
 	const componentsBuffer = Buffer.from(componentsString, 'base64');
