3aaf67273676750b8a965f81ff977f6e9e55843f
nmerget <nmerget@users.noreply.github.com>
2024-02-13 16:52:40 +0000

    deploy: de1f713684ed1540da3cd387877933ea9bd0aec4

---
+++ version/v0.0.64-palette/_next/static/chunks/pages/components/tag/docs/Angular-a9646dd9954fd567.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[41267],{13735:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/tag/docs/Angular",function(){return u(3378)}])}},function(n){n.O(0,[64996,3378,92888,49774,40179],function(){return n(n.s=13735)}),_N_E=n.O()}]);
