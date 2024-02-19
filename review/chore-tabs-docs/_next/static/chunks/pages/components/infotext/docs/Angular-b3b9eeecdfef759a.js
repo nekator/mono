1b853dfe0f69c08bc60b0869d55a593ac8e170ca
nmerget <nmerget@users.noreply.github.com>
2024-02-19 12:23:12 +0000

    deploy: 933bdd133622d040f41306fa3025eb7b0fc310dd

---
+++ review/chore-tabs-docs/_next/static/chunks/pages/components/infotext/docs/Angular-b3b9eeecdfef759a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[24525],{63941:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/infotext/docs/Angular",function(){return u(59146)}])}},function(n){n.O(0,[64996,59146,92888,49774,40179],function(){return n(n.s=63941)}),_N_E=n.O()}]);
