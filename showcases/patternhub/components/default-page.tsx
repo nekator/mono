628836dcc2cb45cfd89e46654280602feb54c6cd
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2024-02-28 15:52:01 +0100

    feat: add anchor headings for patternhub (#2011)
    * feat: add anchor headings for patternhub
    
    * fix: issue with cropped header when clicking on link

--- showcases/patternhub/components/default-page.tsx
+++ showcases/patternhub/components/default-page.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 import {
 	DBBrand,
@@ -7,7 +8,6 @@ import {
 	DBPage,
 	DBSection
 } from '../../../output/react/src';
-import StaticContent from './static-content';
 import Navigation from './navigation';
 import VersionSwitcher from './version-switcher';
 
@@ -16,18 +16,39 @@ const DefaultPage = ({ children }: any) => {
 	const [noH1, setNoH1] = useState<boolean>(false);
 	const [properties, setProperties] = useState<boolean>(false);
 	const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+	const [lastScroll, setLastScroll] = useState<string>();
 	const router = useRouter();
 
 	useEffect(() => {
 		if (router.query) {
-			setFullscreen(router.query.fullscreen === 'true');
-			setNoH1(router.query.noh1 === 'true');
-			setProperties(router.query.properties === 'true');
+			if (router.query.fullscreen) {
+				setFullscreen(router.query.fullscreen === 'true');
+			}
+
+			if (router.query.noh1) {
+				setNoH1(router.query.noh1 === 'true');
+			}
+
+			if (router.query.properties) {
+				setProperties(router.query.properties === 'true');
+			}
+
+			if (router.query.current) {
+				const current: string = Array.isArray(router.query.current)
+					? router.query.current[0]
+					: router.query.current;
+				if (lastScroll !== current) {
+					setLastScroll(current);
+					document
+						.querySelector(`#${current}`)
+						?.scrollIntoView({ behavior: 'smooth' });
+				}
+			}
 		}
 	}, [router]);
 
 	return (
-		<StaticContent>
+		<>
 			{router.isReady && fullscreen && (
 				<div
 					className={`${noH1 ? 'noh1' : ''} ${
@@ -52,32 +73,12 @@ const DefaultPage = ({ children }: any) => {
 									{process.env.NEXT_PUBLIC_APP_NAME}
 								</DBBrand>
 							}
-							slotMetaNavigation={<VersionSwitcher />}
 							slotCallToAction={
-								/* TODO: Use DBSearchBar in future */
 								<DBButton icon="search" variant="text" noText>
 									Search
 								</DBButton>
 							}
-							slotActionBar={
-								<>
-									<DBButton
-										icon="account"
-										variant="text"
-										noText>
-										Profile
-									</DBButton>
-									<DBButton
-										icon="alert"
-										variant="text"
-										noText>
-										Notification
-									</DBButton>
-									<DBButton icon="help" variant="text" noText>
-										Help
-									</DBButton>
-								</>
-							}>
+							slotActionBar={<VersionSwitcher />}>
 							<Navigation />
 						</DBHeader>
 					}>
@@ -86,8 +87,10 @@ const DefaultPage = ({ children }: any) => {
 					</DBSection>
 				</DBPage>
 			)}
-		</StaticContent>
+		</>
 	);
 };
 
-export default DefaultPage;
+export default dynamic(async () => DefaultPage, {
+	ssr: false
+});
--- showcases/patternhub/pages/_app.tsx
+++ showcases/patternhub/pages/_app.tsx
@@ -5,38 +5,75 @@ import '@code-hike/mdx/dist/index.css';
 import '@db-ui/foundations/build/css/colors/classes/all.css';
 import '../styles/globals.scss';
 import '../../showcase-styles.css';
+import LinkHeader from '../components/link-header';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<MDXProvider
-			components={{
-				a: (properties) => (
-					<a
-						target="_blank"
-						referrerPolicy="no-referrer"
-						{...properties}
-					/>
-				),
-				img: (properties) => (
-					<img
-						{...properties}
-						src={
-							properties.src?.startsWith('http')
-								? properties.src
-								: `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}${
-										properties.src
-									}`
-						}
-					/>
-				)
-			}}>
-			<Script
-				src={
-					(process.env.NEXT_PUBLIC_BASE_PATH ?? '') +
-					'/iframe-resizer/iframeResizer.contentWindow.js'
-				}
-			/>
-			<Component {...pageProps} />
-		</MDXProvider>
-	);
-}
+const App = ({ Component, pageProps }: AppProps) => (
+	<MDXProvider
+		components={{
+			h1: (props) => (
+				<h1 {...props}>
+					{props.children}
+					<LinkHeader id={props.id} />
+				</h1>
+			),
+			h2: (props) => (
+				<h2 {...props}>
+					{props.children}
+					<LinkHeader id={props.id} />
+				</h2>
+			),
+			h3: (props) => (
+				<h3 {...props}>
+					{props.children}
+					<LinkHeader id={props.id} />
+				</h3>
+			),
+			h4: (props) => (
+				<h4 {...props}>
+					{props.children}
+					<LinkHeader id={props.id} />
+				</h4>
+			),
+			h5: (props) => (
+				<h5 {...props}>
+					{props.children}
+					<LinkHeader id={props.id} />
+				</h5>
+			),
+			h6: (props) => (
+				<h6 {...props}>
+					{props.children}
+					<LinkHeader id={props.id} />
+				</h6>
+			),
+			a: (properties) => (
+				<a
+					target="_blank"
+					referrerPolicy="no-referrer"
+					{...properties}
+				/>
+			),
+			img: (properties) => (
+				<img
+					{...properties}
+					src={
+						properties.src?.startsWith('http')
+							? properties.src
+							: `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}${
+									properties.src
+								}`
+					}
+				/>
+			)
+		}}>
+		<Script
+			src={
+				(process.env.NEXT_PUBLIC_BASE_PATH ?? '') +
+				'/iframe-resizer/iframeResizer.contentWindow.js'
+			}
+		/>
+		<Component {...pageProps} />
+	</MDXProvider>
+);
+
+export default App;
