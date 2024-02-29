0c4aabdcd642e24ed159e8dd09ece2f5ace21d9e
nmerget <nmerget@users.noreply.github.com>
2024-02-29 07:25:43 +0000

    deploy: 29b154f1534231bb207bfbeb2367855fc1c99390

---
+++ review/fix-2270-icon-alignment/_next/static/chunks/pages/components/brand/docs/Angular-1519dbe904e87031.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[64140],{52450:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/brand/docs/Angular",function(){return u(54368)}])}},function(n){n.O(0,[64996,54368,92888,49774,40179],function(){return n(n.s=52450)}),_N_E=n.O()}]);
---
+++ review/fix-2270-icon-alignment/_next/static/chunks/pages/components/tab/docs/HTML-c1e63a168bc83ddd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[14768],{33919:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/tab/docs/HTML",function(){return u(45152)}])}},function(n){n.O(0,[64996,45152,92888,49774,40179],function(){return n(n.s=33919)}),_N_E=n.O()}]);
