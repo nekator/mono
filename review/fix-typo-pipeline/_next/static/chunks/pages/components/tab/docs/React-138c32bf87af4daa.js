528b08ae0e65c6f1ee008cbad0abe7c142da9b6b
nmerget <nmerget@users.noreply.github.com>
2024-03-01 12:44:13 +0000

    deploy: 68b15fdf70a2c14d8b3d6c4070dd155a099c4a11

---
+++ review/fix-typo-pipeline/_next/static/chunks/pages/components/tab/docs/React-138c32bf87af4daa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[19240],{2703:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/tab/docs/React",function(){return u(91961)}])}},function(n){n.O(0,[64996,91961,92888,49774,40179],function(){return n(n.s=2703)}),_N_E=n.O()}]);
