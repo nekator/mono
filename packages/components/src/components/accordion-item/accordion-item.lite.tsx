838a3736d02e1af9984f3715fadae1a52f5ab3dd
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2024-03-01 11:30:43 +0100

    fix: broken accordion (#2283)

--- packages/components/src/components/accordion-item/accordion-item.lite.tsx
+++ packages/components/src/components/accordion-item/accordion-item.lite.tsx
@@ -1,15 +1,13 @@
 import {
 	onMount,
-	onUpdate,
 	Show,
 	Slot,
 	useMetadata,
 	useRef,
 	useStore
 } from '@builder.io/mitosis';
-import { DBAccordionItemState, DBAccordionItemProps } from './model';
+import { DBAccordionItemProps, DBAccordionItemState } from './model';
 import { cls, uuid } from '../../utils';
-import { DEFAULT_ID } from '../../shared/constants';
 import { ClickEvent } from '../../shared/model';
 
 useMetadata({
@@ -25,20 +23,28 @@ export default function DBAccordionItem(props: DBAccordionItemProps) {
 	const ref = useRef<HTMLDetailsElement>(null);
 	// jscpd:ignore-start
 	const state = useStore<DBAccordionItemState>({
-		_id: DEFAULT_ID,
+		_id: 'accordion-item-' + uuid(),
+		_open: false,
 		toggle: (event: ClickEvent<HTMLElement>) => {
 			// We need this for react https://github.com/facebook/react/issues/15486#issuecomment-488028431
 			event?.preventDefault();
+			const newStateOpen = !state._open;
 			if (props.onToggle) {
-				props.onToggle(!props.open);
+				props.onToggle(newStateOpen);
 			}
+			state._open = newStateOpen;
 		}
 	});
 
 	onMount(() => {
-		state._id = props.id || 'accordion-item-' + uuid();
+		if (props.id) {
+			state._id = props.id;
+		}
 		if (props.stylePath) {
 			state.stylePath = props.stylePath;
+		}
+		if (props.defaultOpen) {
+			state._open = props.defaultOpen;
 		}
 	});
 	// jscpd:ignore-end
@@ -49,15 +55,12 @@ export default function DBAccordionItem(props: DBAccordionItemProps) {
 			id={state._id}
 			class={cls('db-accordion-item', props.className)}
 			aria-disabled={props.disabled}
-			open={props.open}
+			open={state._open}
 			name={props.name}>
 			<Show when={state.stylePath}>
 				<link rel="stylesheet" href={state.stylePath} />
 			</Show>
-			<summary
-				onClick={(event: ClickEvent<HTMLElement>) =>
-					state.toggle(event)
-				}>
+			<summary onClick={(event) => state.toggle(event)}>
 				<Show when={props.title}>{props.title}</Show>
 				<Show when={!props.title}>
 					<Slot name="title" />
--- showcases/angular-showcase/src/app/components/accordion-item/accordion-item.component.ts
+++ showcases/angular-showcase/src/app/components/accordion-item/accordion-item.component.ts
@@ -11,19 +11,4 @@ import { DefaultComponent } from '../default.component';
 })
 export class AccordionItemComponent {
 	variants = defaultComponentVariants;
-
-	openId?: string;
-
-	getId = (exampleIndex: number, variantIndex: number) =>
-		`${variantIndex}${exampleIndex}`;
-
-	toggle = (exampleIndex: number, variantIndex: number) => {
-		this.openId =
-			this.openId === this.getId(exampleIndex, variantIndex)
-				? undefined
-				: this.getId(exampleIndex, variantIndex);
-	};
-
-	isOpen = (exampleIndex: number, variantIndex: number, open?: boolean) =>
-		this.openId === this.getId(exampleIndex, variantIndex) ? true : open;
 }
