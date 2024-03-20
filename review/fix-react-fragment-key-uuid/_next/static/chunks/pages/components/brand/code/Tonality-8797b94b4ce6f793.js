f4e92fee3e59c9d0d9879b3252ddba31c6c6b87a
nmerget <nmerget@users.noreply.github.com>
2024-03-20 00:51:45 +0000

    deploy: a7cec192d8ecb60640a3d76c4272674bfb367878

--- review/fix-react-fragment-key-uuid/_next/static/chunks/pages/components/brand/code/Tonality-8797b94b4ce6f793.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[27539],{69664:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/brand/code/Tonality",function(){return o(20767)}])}},function(n){n.O(0,[64996,20767,92888,49774,40179],function(){return n(n.s=69664)}),_N_E=n.O()}]);
--- review/fix-react-fragment-key-uuid/_next/static/chunks/pages/components/card/code/Colorvariant-662048cb5428235f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[68316],{58721:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/code/Colorvariant",function(){return o(4425)}])}},function(n){n.O(0,[64996,4425,92888,49774,40179],function(){return n(n.s=58721)}),_N_E=n.O()}]);
