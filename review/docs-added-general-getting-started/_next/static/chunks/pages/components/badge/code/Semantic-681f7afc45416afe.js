24dff06673b20f362c8f9a4127ea5e9427ef0ba0
nmerget <nmerget@users.noreply.github.com>
2024-03-20 09:17:13 +0000

    deploy: 94d009eeef9dacf6e9ee15ec20536834e1304af9

---
+++ review/docs-added-general-getting-started/_next/static/chunks/pages/components/badge/code/Semantic-681f7afc45416afe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95869],{67213:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/badge/code/Semantic",function(){return e(31814)}])}},function(n){n.O(0,[64996,31814,92888,49774,40179],function(){return n(n.s=67213)}),_N_E=n.O()}]);
