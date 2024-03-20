9946b6d5deefe0ad25387e1ee643434e48e43eb3
mfranzke <mfranzke@users.noreply.github.com>
2024-03-20 10:21:27 +0000

    deploy: 685aa68d69c37a01311d224cd27c6918aa453a8d

---
+++ review/fix-mobile-sub-navigation/_next/static/chunks/pages/components/infotext/code/Semantic-7a8b2e0215767647.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5035],{71291:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/infotext/code/Semantic",function(){return e(32621)}])}},function(n){n.O(0,[64996,32621,92888,49774,40179],function(){return n(n.s=71291)}),_N_E=n.O()}]);
