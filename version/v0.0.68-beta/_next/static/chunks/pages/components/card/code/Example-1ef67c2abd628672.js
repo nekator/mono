c67fd8fe93691c19bd502348ec6f19695b4a85aa
nmerget <nmerget@users.noreply.github.com>
2024-04-02 08:02:57 +0000

    deploy: 745ca8cc248c0317d1c60a35daf626ef2eb3ea6a

---
+++ version/v0.0.68-beta/_next/static/chunks/pages/components/card/code/Example-1ef67c2abd628672.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[94896],{74185:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/code/Example",function(){return u(75244)}])}},function(n){n.O(0,[14863,75244,92888,49774,40179],function(){return n(n.s=74185)}),_N_E=n.O()}]);
