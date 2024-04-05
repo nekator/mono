cc9015d18b5b2be6e47803808719ae074afd42cb
nmerget <nmerget@users.noreply.github.com>
2024-04-05 07:16:54 +0000

    deploy: 153b712a2aab3dd56b1a1b55f6efb5fe61d8c19e

---
+++ review/feat-new-icons/_next/static/chunks/pages/components/notification/code/ContentVariantStandalone-b050794c80e77ad4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[90651],{22995:function(n,o,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/notification/code/ContentVariantStandalone",function(){return t(28506)}])}},function(n){n.O(0,[14863,28506,92888,49774,40179],function(){return n(n.s=22995)}),_N_E=n.O()}]);
