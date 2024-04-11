39e501afbabe951625dcb32790ba137a872c45a9
nmerget <nmerget@users.noreply.github.com>
2024-04-11 08:01:50 +0000

    deploy: 1d50c27a15e3aa910fbd5b0762586593e58dec97

---
+++ review/test-add-new-compontent-tests/_next/static/chunks/pages/components/accordion/code/Tonality-b6642a569750f917.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61413],{56715:function(n,o,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/accordion/code/Tonality",function(){return _(75942)}])}},function(n){n.O(0,[14863,75942,92888,49774,40179],function(){return n(n.s=56715)}),_N_E=n.O()}]);
