fc81c022f4e034263ff3a349d2c1b344ded43d2b
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-04-25 16:53:21 +0200

    test: add test for home screen

--- showcases/e2e/home/showcase-home.spec.ts
+++ showcases/e2e/home/showcase-home.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 test.describe('Home', () => {
 	test('has title', async ({ page }) => {
 		await page.goto('./');
 		await expect(page).toHaveTitle('Showcase');
 	});
-	// TODO write separate form functionality tests without screenshots
+
+	test('should not have any A11y issues', async ({ page }) => {
+		await page.goto('./');
+		const accessibilityScanResults = await new AxeBuilder({
+			page
+		}).analyze();
+
+		expect(accessibilityScanResults.violations).toEqual([]);
+	});
 });
