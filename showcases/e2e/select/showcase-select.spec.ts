3963a45eb1126dc81ce2d701ae9f3ccb086ef8d4
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-05-13 08:53:23 +0200

    Update showcase-select.spec.ts

--- showcases/e2e/select/showcase-select.spec.ts
+++ showcases/e2e/select/showcase-select.spec.ts
@@ -3,9 +3,7 @@ import { test } from '@playwright/test';
 import { getDefaultScreenshotTest } from '../default.ts';
 
 test.describe('DBSelect', () => {
-	// TODO: Currently disable till we solved https://github.com/db-ui/mono/issues/2587
 	getDefaultScreenshotTest({
-		path: '03/select',
-		disableRules: ['color-contrast']
+		path: '03/select'
 	});
 });
