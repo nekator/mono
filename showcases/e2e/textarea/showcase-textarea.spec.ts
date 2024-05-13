be47b317ed913326b0cbde7b7696b29c5bc09917
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-05-13 08:53:58 +0200

    Update showcase-textarea.spec.ts

--- showcases/e2e/textarea/showcase-textarea.spec.ts
+++ showcases/e2e/textarea/showcase-textarea.spec.ts
@@ -3,9 +3,7 @@ import { test } from '@playwright/test';
 import { getDefaultScreenshotTest } from '../default.ts';
 
 test.describe('DBTextarea', () => {
-	// TODO: Currently disable till we solved https://github.com/db-ui/mono/issues/2587
 	getDefaultScreenshotTest({
-		path: '03/textarea',
-		disableRules: ['color-contrast']
+		path: '03/textarea'
 	});
 });
