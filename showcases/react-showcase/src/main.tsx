8d7105c88ffeddb82017c80eb1bb9c62de94bb66
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2024-05-16 13:23:39 +0200

    test: add sa11y to showcases to find some a11y issues (#2646)

--- showcases/react-showcase/src/main.tsx
+++ showcases/react-showcase/src/main.tsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import '@db-ui/components/build/styles/db-ui-42-rollup.css';
 import '@db-ui/foundations/build/css/colors/classes/all.css';
+import 'sa11y/dist/css/sa11y.min.css';
+import Sa11yLangEn from 'sa11y/dist/js/lang/en.js';
+import { Sa11y, Lang } from 'sa11y/dist/js/sa11y.esm.js';
 import '../../showcase-styles.css';
 import App from './app';
 import { NAVIGATION_ITEMS } from './utils/navigation-item';
+
+if (import.meta.env.DEV) {
+	Lang.addI18n(Sa11yLangEn.strings);
+	const sa11y = new Sa11y({
+		checkRoot: 'body'
+	});
+}
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
 	<React.StrictMode>
