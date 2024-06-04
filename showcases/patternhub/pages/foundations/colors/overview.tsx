0ceadabd83595bb5e2bb17e609196459cc329fc5
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-06-04 18:17:18 +0200

    docs: adds copy to clipboard button for colors

--- showcases/patternhub/pages/foundations/colors/overview.tsx
+++ showcases/patternhub/pages/foundations/colors/overview.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DefaultPage from '../../../components/default-page';
 import { COLORS } from '../../../components/src/shared/constants';
-import { DBInput } from '../../../components/src';
+import { DBInput, DBButton, DBTooltip } from '../../../components/src';
+import CopyClipboardButton from '../../../components/copy-clipboard-button';
 
 const ColorOverview = () => {
 	const [search, setSearch] = useState<string>('');
+	// Const [copiedIndex, setCopiedIndex] = useState<number>(-1);
+	//
+	// const onCopyButtonClick = (
+	// 	event: React.MouseEvent<HTMLButtonElement>,
+	// 	index: number
+	// ) => {
+	// 	const label = (
+	// 		event.target as HTMLButtonElement
+	// 	).parentElement?.querySelector('span');
+	// 	if (label?.textContent && typeof navigator !== 'undefined') {
+	// 		setCopiedIndex(index);
+	// 		navigator.clipboard.writeText(label.textContent);
+	// 	}
+	// };
+	//
+	// useEffect(() => {
+	// 	let timeoutId: NodeJS.Timeout | undefined;
+	//
+	// 	if (copiedIndex > -1) {
+	// 		timeoutId = setTimeout(() => {
+	// 			setCopiedIndex(-1);
+	// 		}, 1500);
+	// 	}
+	//
+	// 	return () => clearTimeout(timeoutId);
+	// }, [copiedIndex]);
+
 	return (
 		<DefaultPage>
 			<h1>Color Overview</h1>
@@ -19,8 +47,15 @@ const ColorOverview = () => {
 			</search>
 			<h2>Overview</h2>
 			<div className="color-overview-container">
-				{COLORS.map((color) => (
-					<div className={`db-${color}`}>{color}</div>
+				{COLORS.map((color, index) => (
+					<div className={`db-${color}`}>
+						<span>db-{color}</span>
+						<CopyClipboardButton
+							name={`copy-button-${index}`}
+							copyText={`db-${color}`}>
+							Copied to clipboard
+						</CopyClipboardButton>
+					</div>
 				))}
 			</div>
 		</DefaultPage>
