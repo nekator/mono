bc172fe9580bcf1de69d19f49767ecb00f46e7a2
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-06-20 17:09:14 +0200

    fix: linting issue

--- showcases/react-showcase/src/main.tsx
+++ showcases/react-showcase/src/main.tsx
@@ -11,7 +11,7 @@ import { NAVIGATION_ITEMS } from './utils/navigation-item';
 
 // Makes env-variable available in the components without using import.meta.
 // as a result, the components are also compatible with next.
-// eslint-disable @typescript-eslint/dot-notation
+// eslint-disable-next-line @typescript-eslint/dot-notation
 window['env'] = {
 	BASE_URL: import.meta.env.BASE_URL
 };
