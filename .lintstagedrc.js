53af477331622cf34059578459b2243dcffabbd9
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2024-07-18 06:48:53 +0200

    refactor: prettify all files
    adapted out of GH typicode/husky/releases/tag/v9.1.0#:~:text=to%20the%20docs.-,Lint%20staged%20files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,5 @@
 export default {
 	'*.md': 'markdownlint -c .markdown-lint.yml',
 	'*.{css,scss}': 'stylelint --fix --allow-empty-input',
-	'*.{js,ts,tsx,jsx,mjs,cjs}': ['prettier --write', 'xo --fix'],
-	'!*.{js,ts,tsx,jsx,mjs,cjs}': 'prettier --write --ignore-unknown'
+	'*.{js,ts,tsx,jsx,mjs,cjs}': ['xo --fix']
 };
