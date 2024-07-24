b6ed7a415934c55d7b9af94a6f2ccabcf0e4701d
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2024-07-24 13:40:50 +0200

    test: add guidepup as screen-reader automation (#2556)
    * test: add guidepup as screen-reader automation
    
    * test: screen-reader in pipeline
    
    * test: screen-reader
    
    * chore: update frm evaluations
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * chore: update screen-reader tests
    
    * fix: screen reader workflow for ci
    
    * fix: screen reader workflow for ci
    
    * chore: update from main
    
    * fix: issues with typescript
    
    * chore: changed tests for guidedpup
    
    * fix: issue with angular
    
    * fix: snapshots
    
    * chore: update from main
    
    * chore: update guidepup tests
    
    * fix: issue with download showcase
    
    * chore: remove webkit from screen reader test
    
    * fix: path for recordings
    
    * fix: issue with line ending
    
    * test: if voiceover.next works for labels
    
    * fix: issue with voiceOver test
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup
    
    * fix: linting issue
    
    * fix: linting issue
    
    * test: guidepup
    
    * test: guidepup
    
    * test: guidepup

---
+++ showcases/screen-reader/README.md
@@ -0,0 +1,47 @@
+# Screen Automated Reader (ScAR 🦁🔥💀)
+
+Start a test with these commands:
+
+## MacOS
+
+```shell
+npm run test:screen-reader:macos --workspace=react-showcase -- --ui
+```
+
+## Windows
+
+```shell
+npm run test:screen-reader:windows --workspace=react-showcase -- --ui
+```
+
+## Gotchas
+
+-   Local: Don't switch in between your windows while testing, it will capture only your current screen
+-   We should avoid auto-generate tests, because they take a lot of time.
+-   NVDAs `next` command is equivalent of executing Down Arrow - Won't work with radio/select as you might expect
+-   One simple test takes about 1 minute in CI ⬅ so you should only provide test important things
+
+## More information
+
+We use this [survey](https://webaim.org/projects/screenreadersurvey10/) to reduce amount of tests (only for VoiceOver and NVDA).
+
+> Most common screen reader and browser combinations:
+
+| Screen Reader & Browser | # of Respondents | % of Respondents |
+| ----------------------- | ---------------- | ---------------- |
+| NVDA with Chrome        | 323              | 21.3%            |
+| NVDA with Firefox       | 152              | 10.0%            |
+| VoiceOver with Safari   | 107              | 7.0%             |
+| NVDA with Edge          | 75               | 5.0%             |
+| VoiceOver with Chrome   | 30               | 2.0%             |
+
+> What operating system are you on when using your primary desktop/laptop screen reader?
+
+| Response | # of respondents | % of respondents |
+| -------- | ---------------- | ---------------- |
+| Windows  | 1311             | 86.1%            |
+| Mac      | 146              | 9.6%             |
+| Linux    | 44               | 2.9%             |
+| Other    | 21               | 1.4%             |
+
+Conclusion: We only test Chrome for Windows and Safari for MacOS because these are the most common combinations.
---
+++ showcases/screen-reader/default.ts
@@ -0,0 +1,152 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { platform } from 'node:os';
+import {
+	type NVDAPlaywright,
+	nvdaTest,
+	type VoiceOverPlaywright,
+	voiceOverTest
+} from '@guidepup/playwright';
+import { macOSRecord, windowsRecord } from '@guidepup/guidepup';
+import { expect } from '@playwright/test';
+import {
+	type DefaultTestType,
+	type RunTestType,
+	type ScreenReaderTestType
+} from './data';
+
+const translations: Record<string, string[]> = {
+	button: ['Schalter'],
+	edit: ['Eingabefeld'],
+	'radio button': ['Auswahlschalter'],
+	blank: ['Leer'],
+	checked: ['aktiviert'],
+	' of ': [' von '],
+	clickable: ['anklickbar'],
+	'has auto complete': ['mit Auto Vervollständigung']
+};
+
+const cleanSpeakInstructions = (phraseLog: string[]): string[] =>
+	phraseLog.map((phrase) =>
+		phrase
+			.split('. ')
+			.filter(
+				(sPhrase) =>
+					!(
+						sPhrase.includes('You are currently') ||
+						sPhrase.includes('To enter') ||
+						sPhrase.includes('To exit') ||
+						sPhrase.includes('To click') ||
+						sPhrase.includes('To select') ||
+						sPhrase.includes('To interact') ||
+						sPhrase.includes('Press Control')
+					)
+			)
+			.join('. ')
+	);
+
+export const generateSnapshot = async (
+	screenReader?: VoiceOverPlaywright | NVDAPlaywright,
+	retry?: number
+) => {
+	if (!screenReader) return;
+
+	let phraseLog: string[] = await screenReader.spokenPhraseLog();
+
+	if (retry && retry > 0) {
+		process.stdout.write(JSON.stringify(phraseLog));
+	}
+
+	phraseLog = cleanSpeakInstructions(phraseLog);
+
+	let snapshot = JSON.stringify(phraseLog);
+
+	for (const [key, values] of Object.entries(translations)) {
+		for (const value of values) {
+			snapshot = snapshot.replaceAll(value, key);
+		}
+	}
+
+	expect(snapshot).toMatchSnapshot();
+};
+
+export const runTest = async ({
+	title,
+	url,
+	testFn,
+	postTestFn,
+	additionalParams,
+	page,
+	nvda,
+	voiceOver,
+	retry
+}: DefaultTestType & RunTestType) => {
+	await page.goto(`${url}${additionalParams}`, {
+		waitUntil: 'networkidle'
+	});
+	await page.waitForTimeout(500);
+
+	let recorder: (() => void) | undefined;
+
+	if (retry > 0) {
+		const path = `./recordings/${title}-${retry}-${Date.now()}.mp4`;
+		recorder = isWin() ? windowsRecord(path) : macOSRecord(path);
+	}
+
+	const screenRecorder: VoiceOverPlaywright | NVDAPlaywright | undefined =
+		nvda ?? voiceOver;
+	if (!screenRecorder) return;
+
+	/**
+	 * In macOS:Webkit the [automaticallySpeakWebPage](https://github.com/guidepup/guidepup/blob/main/src/macOS/VoiceOver/configureSettings.ts#L58) is active.
+	 * Therefore, we need to move back with the cursor to the start and delete the logs before starting.
+	 * In windows:Chrome the cursor is on the middle element.
+	 * Therefore, we need to move back and delete the logs, and then start everything.
+	 */
+	await screenRecorder.navigateToWebContent();
+	await page.waitForTimeout(500);
+
+	await testFn?.(voiceOver, nvda);
+	await postTestFn?.(voiceOver, nvda, retry);
+	recorder?.();
+};
+
+export const testDefault = (defaultTestType: DefaultTestType) => {
+	const { test, title, additionalParams, postTestFn } = defaultTestType;
+	const fallbackPostFn = async (voiceOver, nvda, retry) => {
+		await generateSnapshot(voiceOver ?? nvda, retry);
+	};
+
+	const testType: DefaultTestType = {
+		...defaultTestType,
+		postTestFn: postTestFn ?? fallbackPostFn,
+		additionalParams:
+			additionalParams ?? '&color=neutral-bg-lvl-1&density=regular'
+	};
+
+	if (isWin()) {
+		test?.(title, async ({ page, nvda }, { retry }) => {
+			await runTest({
+				...testType,
+				page,
+				nvda,
+				retry
+			});
+		});
+	} else {
+		test?.(title, async ({ page, voiceOver }, { retry }) => {
+			await runTest({
+				...testType,
+				page,
+				voiceOver,
+				retry
+			});
+		});
+	}
+};
+
+const isWin = (): boolean => platform() === 'win32';
+
+export const getTest = (): ScreenReaderTestType =>
+	isWin() ? nvdaTest : voiceOverTest;
+
+export default { testDefault, generateSnapshot, getTest };
