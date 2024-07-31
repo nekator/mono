2a35910cf48504eabd2da40fc24322cedd0a0a47
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-07-31 07:40:42 +0200

    chore: update colors with migration tool

--- packages/foundations/scripts/migration/color-update.ts
+++ packages/foundations/scripts/migration/color-update.ts
@@ -4,67 +4,67 @@ export const colorUpdateQ32024: ReplaceInFileConfig[] = [
 	// Basic backgrounds
 	{
 		files: '',
-		from: 'bg-lvl-1',
+		from: /bg-lvl-1/g,
 		to: 'bg-basic-level-1'
 	},
 	{
 		files: '',
-		from: 'bg-lvl-2',
+		from: /bg-lvl-2/g,
 		to: 'bg-basic-level-2'
 	},
 	{
 		files: '',
-		from: 'bg-lvl-3',
+		from: /bg-lvl-3/g,
 		to: 'bg-basic-level-3'
 	},
 	{
 		files: '',
-		from: 'bg-transparent-full',
+		from: /bg-transparent-full/g,
 		to: 'bg-basic-transparent-full'
 	},
 	{
 		files: '',
-		from: 'bg-transparent-semi',
+		from: /bg-transparent-semi/g,
 		to: 'bg-basic-transparent-semi'
 	},
 	{
 		files: '',
-		from: 'on-bg-enabled',
+		from: /on-bg-enabled/g,
 		to: 'on-bg-basic-100-default'
 	},
 	{
 		files: '',
-		from: 'on-bg-hover',
+		from: /on-bg-hover/g,
 		to: 'on-bg-basic-100-hovered'
 	},
 	{
 		files: '',
-		from: 'on-bg-pressed',
+		from: /on-bg-pressed/g,
 		to: 'on-bg-basic-100-pressed'
 	},
 	{
 		files: '',
-		from: 'on-bg-weak-enabled',
+		from: /on-bg-weak-enabled/g,
 		to: 'on-bg-basic-90-default'
 	},
 	{
 		files: '',
-		from: 'on-bg-weak-hover',
+		from: /on-bg-weak-hover/g,
 		to: 'on-bg-basic-90-hovered'
 	},
 	{
 		files: '',
-		from: 'on-bg-weak-pressed',
+		from: /on-bg-weak-pressed/g,
 		to: 'on-bg-basic-90-pressed'
 	},
 	{
 		files: '',
-		from: 'contrast-low-enabled',
+		from: /contrast-low-enabled/g,
 		to: 'on-bg-basic-70-default'
 	},
 	{
 		files: '',
-		from: 'color-border',
+		from: /color-border/g,
 		// We will replace adaptive-color afterward
 		to: 'color-on-bg-basic-60-default'
 	},
@@ -72,50 +72,50 @@ export const colorUpdateQ32024: ReplaceInFileConfig[] = [
 	// Contrast -> inverted
 	{
 		files: '',
-		from: 'on-contrast-enabled',
+		from: /on-contrast-enabled/g,
 		to: 'on-bg-inverted-default'
 	},
 	{
 		files: '',
-		from: 'on-contrast-hover',
+		from: /on-contrast-hover/g,
 		to: 'on-bg-inverted-hovered'
 	},
 	{
 		files: '',
-		from: 'on-contrast-pressed',
+		from: /on-contrast-pressed/g,
 		to: 'on-bg-inverted-pressed'
 	},
 	{
 		files: '',
-		from: 'contrast-high-enabled',
+		from: /contrast-high-enabled/g,
 		to: 'bg-inverted-contrast-high-default'
 	},
 	{
 		files: '',
-		from: 'contrast-high-hover',
+		from: /contrast-high-hover/g,
 		to: 'bg-inverted-contrast-high-hovered'
 	},
 	{
 		files: '',
-		from: 'contrast-high-pressed',
+		from: /contrast-high-pressed/g,
 		to: 'bg-inverted-contrast-high-pressed'
 	},
 
 	// Rest
 	{
 		files: '',
-		from: '-enabled',
+		from: /-enabled/g,
 		to: '-default'
 	},
 	{
 		files: '',
 		// Revert changes back
-		from: '-hovereded',
+		from: /-hovereded/g,
 		to: '-hovered'
 	},
 	{
 		files: '',
-		from: 'current-color',
+		from: /current-color/g,
 		to: 'adaptive'
 	}
 ];
