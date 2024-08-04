8c990df8b599fc8e08179c38ccbf25787fc00fd0
nmerget <nmerget@users.noreply.github.com>
2024-08-04 01:05:20 +0000

    deploy: 5bf5f133bd7b3cf399232afa703e3948cb9bf97e

--- version/v0.0.66-palette/_next/static/chunks/pages/components/card/code/Example-a257d185aa5adfd2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[94896],{58464:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/card/code/Example",function(){return u(72521)}])}},function(n){n.O(0,[64996,72521,92888,49774,40179],function(){return n(n.s=58464)}),_N_E=n.O()}]);
--- version/v0.0.66-palette/_next/static/chunks/pages/components/notification/code/ContentVariantStandalone-e1db1657b00a3b2a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[90651],{47544:function(n,o,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/notification/code/ContentVariantStandalone",function(){return t(28150)}])}},function(n){n.O(0,[64996,28150,92888,49774,40179],function(){return n(n.s=47544)}),_N_E=n.O()}]);
