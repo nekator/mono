5368d69f761fe63f1d0b5be5174113e856e5dc52
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-08-06 16:55:10 +0200

    test: adds guidepup tests for accordion

--- showcases/screen-reader/tests/accordion.spec.ts
+++ showcases/screen-reader/tests/accordion.spec.ts
@@ -5,7 +5,7 @@ const test = getTest();
 test.describe('DBAccordion', () => {
 	testDefault({
 		test,
-		title: 'should ... (next)',
+		title: 'should open two items while both remain unfolded (next)',
 		url: './#/04/accordion?page=behaviour',
 		async testFn(voiceOver, nvda) {
 			if (nvda) {
@@ -14,9 +14,40 @@ test.describe('DBAccordion', () => {
 
 			const screenReader = voiceOver ?? nvda;
 			await screenReader?.clearSpokenPhraseLog();
-			await screenReader?.previous();
+			await screenReader?.next(); // Focus: "item 1"
+			await screenReader?.act(); // Interact: "item 1"
+			await screenReader?.next(); // Focus: "content 1"
+			await screenReader?.next(); // Focus: "item 2"
+			await screenReader?.act(); // Interact: "item 2"
+			await screenReader?.next(); // Focus: "content 2"
+			await screenReader?.previous(); // Focus: "item 2"
+			await screenReader?.previous(); // Focus: "content 1"
+			await screenReader?.previous(); // Focus: "item 1"
+		}
+	});
+	testDefault({
+		test,
+		title: 'one item should open, then the next, whereby the first should then close automatically (next)',
+		url: './#/04/accordion?page=behaviour',
+		async testFn(voiceOver, nvda) {
+			if (nvda) {
+				await nvda?.next();
+			}
+
+			const screenReader = voiceOver ?? nvda;
 			await screenReader?.next();
 			await screenReader?.next();
+			await screenReader?.next();
+			await screenReader?.next();
+			await screenReader?.clearSpokenPhraseLog();
+			await screenReader?.next(); // Focus example "Single","Item 1"
+			await screenReader?.act(); // Interact: "item 1"
+			await screenReader?.next(); // Focus: "content 1"
+			await screenReader?.next(); // Focus: "item 2"
+			await screenReader?.act(); // Interact: "item 2"
+			await screenReader?.next(); // Focus: "content 2"
+			await screenReader?.previous(); // Focus: "item 2"
+			await screenReader?.previous(); // Focus: "item 1"
 		}
 	});
 });
