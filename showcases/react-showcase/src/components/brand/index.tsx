00d361871ae7b2ac77b01df85d8b44805b89d2b3
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-08-13 12:20:06 +0200

    Update index.tsx

--- showcases/react-showcase/src/components/brand/index.tsx
+++ showcases/react-showcase/src/components/brand/index.tsx
@@ -16,7 +16,7 @@ const getBrand = ({
 				src={`${
 					process?.env?.NEXT_PUBLIC_BASE_PATH ?? '/react-showcase'
 				}/assets/images/placeholder.jpg`}
-				alt="Logo 1"
+				alt="Logo 2"
 			/>
 		)}
 		{children}
