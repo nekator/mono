6e757b351b97773a0853f9a7893e2d5b3b91ad47
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2024-08-13 13:17:28 +0200

    Revert "refactor: one last try"
    This reverts commit 1dc45daeeac310e85012544fb831cc4eb444b0e7.

--- showcases/react-showcase/src/components/brand/index.tsx
+++ showcases/react-showcase/src/components/brand/index.tsx
@@ -16,7 +16,7 @@ const getBrand = ({
 				src={`${
 					process?.env?.NEXT_PUBLIC_BASE_PATH ?? '/react-showcase'
 				}/assets/images/placeholder.jpg`}
-				alt="Logoo1"
+				alt="this is a fancy placeholder logo"
 			/>
 		)}
 		{children}
