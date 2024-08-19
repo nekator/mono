7a38edafb0aba6095a3e3a0dafa746f7d99b4ce1
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-19 14:27:30 +0200

    chore: update sr test for nvda navigation

--- showcases/screen-reader/tests/navigation.spec.ts
+++ showcases/screen-reader/tests/navigation.spec.ts
@@ -11,25 +11,31 @@ test.describe('DBNavigation', () => {
 		url: './#/05/navigation?page=density',
 		async testFn(voiceOver, nvda) {
 			if (nvda) {
-				await nvda?.next();
+				await nvda?.next(); // Navigation Item 1
+				await nvda?.act(); // Open menu
+				await nvda?.next(); // Sub-Navigation Item 1
+				await nvda?.act(); // Open menu
+				await nvda?.next(); // Sub-Sub-Navigation Item 1
+				await nvda?.next(); // Sub-Sub-Navigation Item 2
+				await nvda?.next(); // Sub-Navigation Item 2
+				await nvda?.next(); // Navigation Item 2
+				await nvda?.next(); // Navigation Item 3
+			} else if (voiceOver) {
+				await voiceOver?.next(); // Navigation "Functional"
+				await voiceOver?.next(); // List A with 3 items
+				await voiceOver?.next(); // 		Menu "Navi-Item 1" - current page
+				await voiceOver?.next(); // 		List B with 2 items
+				await voiceOver?.next(); // 			Menu "Sub-Navi-Item 1" - current page
+				await voiceOver?.next(); // 			List C with 2 items
+				await voiceOver?.next(); // 				Link "Sub-Sub-Navi-Item 1" - current page
+				await voiceOver?.next(); // 				Link "Sub-Sub-Navi-Item 2"
+				await voiceOver?.next(); // 			List C end
+				await voiceOver?.next(); // 			Link "Sub-Navi-Item 2"
+				await voiceOver?.next(); // 		List B end
+				await voiceOver?.next(); // 		Link "Navi-Item 2"
+				await voiceOver?.next(); // 		Link "Navi-Item 3" - dimmed
+				await voiceOver?.next(); // List A end
 			}
-
-			const screenReader = voiceOver ?? nvda;
-			await screenReader?.clearSpokenPhraseLog();
-			await screenReader?.next(); // Navigation "Functional"
-			await screenReader?.next(); // List A with 3 items
-			await screenReader?.next(); // 		Menu "Navi-Item 1" - current page
-			await screenReader?.next(); // 		List B with 2 items
-			await screenReader?.next(); // 			Menu "Sub-Navi-Item 1" - current page
-			await screenReader?.next(); // 			List C with 2 items
-			await screenReader?.next(); // 				Link "Sub-Sub-Navi-Item 1" - current page
-			await screenReader?.next(); // 				Link "Sub-Sub-Navi-Item 2"
-			await screenReader?.next(); // 			List C end
-			await screenReader?.next(); // 			Link "Sub-Navi-Item 2"
-			await screenReader?.next(); // 		List B end
-			await screenReader?.next(); // 		Link "Navi-Item 2"
-			await screenReader?.next(); // 		Link "Navi-Item 3" - dimmed
-			await screenReader?.next(); // List A end
 		}
 	});
 });
