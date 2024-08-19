57cf217c280a359ff124ef1a3493bab09788fbba
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-19 13:10:06 +0200

    chore: update sr test for nvda link

--- showcases/screen-reader/tests/link.spec.ts
+++ showcases/screen-reader/tests/link.spec.ts
@@ -10,12 +10,7 @@ test.describe('DBLink', () => {
 			'should not mention icon, should have link and dimmed link (next())',
 		url: './#/02/link?page=interaction-states',
 		async testFn(voiceOver, nvda) {
-			if (nvda) {
-				await nvda?.next();
-			}
-
 			const screenReader = voiceOver ?? nvda;
-			await screenReader?.clearSpokenPhraseLog();
 			await screenReader?.previous(); // Link "Enabled (Default)/Hover/Pressed"
 			await screenReader?.next(); // Link dimmed "Disabled"
 		}
