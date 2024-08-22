abb174a33ee88e6e4515e0d2f44609f540f2d1f9
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-08-22 18:04:21 +0200

    test: adds guidepup test for section

---
+++ showcases/screen-reader/tests/section.spec.ts
@@ -0,0 +1,23 @@
+import { getTest, testDefault } from '../default';
+
+const test = getTest();
+
+test.describe('DBSection', () => {
+	testDefault({
+		test,
+		title: 'default',
+		description: 'should have ...? (next())',
+		url: './#/01/section?page=width',
+		async testFn(voiceOver, nvda) {
+			if (nvda) {
+				await nvda?.next();
+			}
+
+			const screenReader = voiceOver ?? nvda;
+			await screenReader?.clearSpokenPhraseLog();
+			await screenReader?.next(); // Section 1
+			await screenReader?.next(); // Section 2
+			await screenReader?.next(); // Section 3
+		}
+	});
+});
