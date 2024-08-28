48c568633d77940a0cf2a88fe1dfcdeacff40d35
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-08-28 14:26:15 +0200

    test: adds guidepup test for switch no icon

--- showcases/screen-reader/tests/switch.spec.ts
+++ showcases/screen-reader/tests/switch.spec.ts
@@ -6,8 +6,7 @@ test.describe('DBSwitch', () => {
 	testDefault({
 		test,
 		title: 'default',
-		description:
-			'should have texts inline or as data-label attributes (next())',
+		description: 'should toggle, should not toggle disabled',
 		url: './#/03/switch?page=states',
 		async testFn(voiceOver, nvda) {
 			if (nvda) {
@@ -25,6 +24,25 @@ test.describe('DBSwitch', () => {
 			await screenReader?.next(); // Focus "switch 3"
 			await screenReader?.act(); // Interact "switch 1"
 			await screenReader?.next(); // Focus "switch 3 inline text"
+		}
+	});
+	testDefault({
+		test,
+		title: 'icon',
+		description: 'should not announce icons',
+		url: './#/03/switch?page=visual+aid',
+		async testFn(voiceOver, nvda) {
+			if (nvda) {
+				await nvda?.next();
+			}
+
+			const screenReader = voiceOver ?? nvda;
+			await screenReader?.clearSpokenPhraseLog();
+			await screenReader?.previous(); // Focus "switch 1"
+			await screenReader?.next(); // Focus "switch 1 inline text"
+			await screenReader?.next(); // Focus "switch 2"
+			await screenReader?.act(); // Interact "switch 2"
+			await screenReader?.next(); // Focus "switch 2"
 		}
 	});
 });
