9ea067234bc72e079e8617fd2104516b0185a8af
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-29 09:22:32 +0200

    chore: update sr test for tabs

--- showcases/screen-reader/tests/tabs.spec.ts
+++ showcases/screen-reader/tests/tabs.spec.ts
@@ -11,21 +11,29 @@ test.describe('DBTabs', () => {
 		url: './#/04/tabs?page=tonality',
 		async testFn(voiceOver, nvda) {
 			if (nvda) {
-				await nvda?.next();
+				// We want to lose focus for radio buttons otherwise we can't jump to tab panel
+				await nvda.perform(
+					nvda.keyboardCommands.toggleBetweenBrowseAndFocusMode
+				);
+				await nvda?.perform(nvda.keyboardCommands.exitFocusMode);
+				await nvda?.clearSpokenPhraseLog();
+				await nvda?.next(); // Focus "tab 2"
+				await nvda?.act(); // Select "tab 2"
+				await nvda?.next(); // Focus "tab 3"
+				await nvda?.next(); // Focus "tab panel 3"
+			} else if (voiceOver) {
+				await voiceOver?.next();
+				await voiceOver?.clearSpokenPhraseLog();
+				await voiceOver?.next(); // Focus "tab 1"
+				await voiceOver?.next(); // Focus "tab 1 inline-text"
+				await voiceOver?.next(); // Focus "tab 2"
+				await voiceOver?.act(); // Select "tab 2"
+				await voiceOver?.next(); // Focus "tab 2 inline-text"
+				await voiceOver?.next(); // Focus "tab 3"
+				await voiceOver?.next(); // Focus "tab 3 inline-text"
+				await voiceOver?.next(); // Focus "tab panel 3"
+				await voiceOver?.next(); // Focus "tab panel 3 inline-text"
 			}
-
-			const screenReader = voiceOver ?? nvda;
-			await screenReader?.next(); //
-			await screenReader?.clearSpokenPhraseLog();
-			await screenReader?.next(); // Focus "tab 1"
-			await screenReader?.next(); // Focus "tab 1 inline-text"
-			await screenReader?.next(); // Focus "tab 2"
-			await screenReader?.act(); // Select "tab 2"
-			await screenReader?.next(); // Focus "tab 2 inline-text"
-			await screenReader?.next(); // Focus "tab 3"
-			await screenReader?.next(); // Focus "tab 3 inline-text"
-			await screenReader?.next(); // Focus "tab panel 3"
-			await screenReader?.next(); // Focus "tab panel 3 inline-text"
 		}
 	});
 });
