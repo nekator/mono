57f809f1d63f0d28a283d4cf2ef644b1dd774c23
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-29 09:34:48 +0200

    chore: update sr test for tag

--- showcases/screen-reader/tests/tag.spec.ts
+++ showcases/screen-reader/tests/tag.spec.ts
@@ -9,10 +9,6 @@ test.describe('DBTag', () => {
 		description: 'should announce inline-texts',
 		url: './#/04/tag?&page=density',
 		async testFn(voiceOver, nvda) {
-			if (nvda) {
-				await nvda?.next();
-			}
-
 			const screenReader = voiceOver ?? nvda;
 			await screenReader?.clearSpokenPhraseLog();
 			await screenReader?.previous(); // Focus "tag 1"
@@ -26,23 +22,28 @@ test.describe('DBTag', () => {
 		description: 'should announce inline-texts',
 		url: './#/04/tag?page=behaviour',
 		async testFn(voiceOver, nvda) {
-			if (nvda) {
-				await nvda?.next();
-			}
-
 			const screenReader = voiceOver ?? nvda;
 			await screenReader?.clearSpokenPhraseLog();
-			await screenReader?.next(); // Focus "default static"
+			await (voiceOver ? screenReader?.next() : screenReader?.previous()); // Focus "default static"
 			await screenReader?.next(); // Focus "button"
 			await screenReader?.next(); // Focus "link"
 			await screenReader?.next(); // Focus "checkbox"
 			await screenReader?.act(); // Select "checkbox"
-			await screenReader?.next(); // Focus "checkbox inline-text"
+			if (voiceOver) {
+				await screenReader?.next(); // Focus "checkbox inline-text"
+			}
+
 			await screenReader?.next(); // Focus "radio 1"
 			await screenReader?.act(); // Select "radio 1"
-			await screenReader?.next(); // Focus "radio 1 inline-text"
+			if (voiceOver) {
+				await screenReader?.next(); // Focus "radio 1 inline-text"
+			}
+
 			await screenReader?.next(); // Focus "radio 2"
-			await screenReader?.next(); // Focus "radio 2 inline-text"
+			if (voiceOver) {
+				await screenReader?.next(); // Focus "radio 2 inline-text"
+			}
+
 			await screenReader?.next(); // Focus "removable"
 			await screenReader?.next(); // Focus "remove button"
 			await screenReader?.act(); // Interact "remove button"
