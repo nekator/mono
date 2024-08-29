0d019732abf1d17906fa907ea369baff7e966558
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-29 07:48:51 +0200

    chore: update sr test for section

--- showcases/screen-reader/tests/section.spec.ts
+++ showcases/screen-reader/tests/section.spec.ts
@@ -10,13 +10,15 @@ test.describe('DBSection', () => {
 		url: './#/01/section?page=width',
 		async testFn(voiceOver, nvda) {
 			if (nvda) {
-				await nvda?.next();
+				/* We don't have a focusable element, so we are initially on the browser (i) button */
+				await nvda.press('Shift+Tab'); // Jump into the website -> Section 2
+				await nvda?.clearSpokenPhraseLog();
+				await nvda?.previous(); // Section 1
+				await nvda?.next(); // Section 2
+			} else if (voiceOver) {
+				await voiceOver?.previous(); // Section 1
+				await voiceOver?.next(); // Section 2
 			}
-
-			const screenReader = voiceOver ?? nvda;
-			await screenReader?.clearSpokenPhraseLog();
-			await screenReader?.previous(); // Section 1
-			await screenReader?.next(); // Section 2
 		}
 	});
 });
