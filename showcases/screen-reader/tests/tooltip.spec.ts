fa6d5e22e10de1ee8bab0000ef9f51356808d3e8
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-29 11:23:33 +0200

    chore: update sr test for tooltip

--- showcases/screen-reader/tests/tooltip.spec.ts
+++ showcases/screen-reader/tests/tooltip.spec.ts
@@ -6,17 +6,16 @@ test.describe('DBTooltip', () => {
 	testDefault({
 		test,
 		title: 'default',
-		description: 'should have buttons with tooltips (next())',
+		description: 'should have buttons with tooltips',
 		url: './#/04/tooltip?page=density',
 		async testFn(voiceOver, nvda) {
 			if (nvda) {
-				await nvda?.next();
+				await nvda?.press('Tab'); // Focus "button 2 with tooltip"
+				await nvda?.press('Tab'); // Focus "button 3 with tooltip"
+			} else if (voiceOver) {
+				await voiceOver?.previous(); // Focus "button 1 with tooltip"
+				await voiceOver?.next(); // Focus "button 2 with tooltip"
 			}
-
-			const screenReader = voiceOver ?? nvda;
-			await screenReader?.clearSpokenPhraseLog();
-			await screenReader?.previous(); // Focus "button 1 with tooltip"
-			await screenReader?.next(); // Focus "button 2 with tooltip"
 		}
 	});
 });
