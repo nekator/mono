e8fcdbf76fde48bfe62c2c7a40ad2587fffc4edd
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-30 07:44:30 +0200

    chore: update snapshot

--- showcases/screen-reader/tests/textarea.spec.ts
+++ showcases/screen-reader/tests/textarea.spec.ts
@@ -54,7 +54,9 @@ test.describe('DBTextarea', () => {
 				 * There is a timing issue for macOS for typing in input we clean the result
 				 */
 				await generateSnapshot(voiceOver, retry, (phraseLog) =>
-					phraseLog.map((log) => log.replace('t. ', ''))
+					phraseLog.map((log) =>
+						log.replace('Test. ', '').replace('t. ', '')
+					)
 				);
 			}
 		}
