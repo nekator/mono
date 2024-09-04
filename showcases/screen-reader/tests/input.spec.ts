45e250a7b6684fa78b82da35402012eb6f7bc6fc
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-09-04 11:31:16 +0200

    chore: fix input test

--- showcases/screen-reader/tests/input.spec.ts
+++ showcases/screen-reader/tests/input.spec.ts
@@ -70,7 +70,7 @@ test.describe('DBInput', () => {
 				/*
 				 * There is a timing issue for macOS for typing in input we clean the result
 				 */
-				await generateSnapshot(nvda, retry, (phraseLog) =>
+				await generateSnapshot(voiceOver, retry, (phraseLog) =>
 					phraseLog.map((log) => log.replace('t. ', ''))
 				);
 			}
