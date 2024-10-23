82f33d93aeac01891021e8117fabaa577a49e8af
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-10-23 13:29:12 +0200

    refactor: revert previous solution (#3357)
    * Revert "refactor: embed our page into an iframe instead of doing a redirect (#3291)"
    
    This reverts commit 9d3c4ec91318df265ea3102381fccefad56c9fec.
    
    * Revert "refactor: embed our page into an iframe instead of doing a redirect (#3291)"
    
    This reverts commit 9d3c4ec91318df265ea3102381fccefad56c9fec.

--- .github/scripts/build-gh-page.sh
+++ .github/scripts/build-gh-page.sh
@@ -16,7 +16,7 @@ tar -zxf gh-pages -C public --strip-components 1
 
 if [[ $RELEASE == "true" ]]; then
 	echo "🔃 Change redirect"
-	echo "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>DB UX Design System Core – latest version</title></head><body style=\"margin: 0;\"><iframe src=\"https://"$OWNER_NAME".github.io/"$REPO_NAME"/version/"$NAME"\" style=\"border: 0;width: 100vw;height: 100vh;\"></iframe></body></html>" > public/index.html
+	echo "<meta http-equiv=\"refresh\" content=\"0; URL=https://"$OWNER_NAME".github.io/"$REPO_NAME"/version/"$NAME"\" />" > public/index.html
 fi
 
 echo "👣 Move out dir"
