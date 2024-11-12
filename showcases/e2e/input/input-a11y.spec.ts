65a8c7f1eddc1f206eac9877b516b9d637b620f1
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-11-12 10:38:01 +0100

    refactor(axe): removed obsolete disabled rule (#3449)
    the underlaying issue was resolved by IBM equal access team.

--- showcases/e2e/input/input-a11y.spec.ts
+++ showcases/e2e/input/input-a11y.spec.ts
@@ -6,7 +6,6 @@ test.describe('DBInput', () => {
 	// TODO: Currently disable till we solved https://github.com/db-ui/mono/issues/2587
 	getA11yTest({
 		path: '03/input',
-		axeDisableRules: ['color-contrast'],
-		aCheckerDisableRules: ['label_ref_valid'] // TODO: There is an issue with datetime-local - https://github.com/IBMa/equal-access/issues/1910
+		axeDisableRules: ['color-contrast']
 	});
 });
